0d10e79a4f54b154796d90d7aed730a06d1026c3
Erkus284 <78791268+Erkus284@users.noreply.github.com>
2021-02-09 17:23:08 +0100

    New changes on map

---
+++ pipo-map001_at-kusa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pipo-map001_at-kusa" tilewidth="32" tileheight="32" tilecount="5" columns="1">
+ <image source="../../../../../Program Files/Tiled/examples/32x32/[A]_type1/pipo-map001_at-kusa.png" width="32" height="160"/>
+</tileset>
